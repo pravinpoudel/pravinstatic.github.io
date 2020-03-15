8cea64c1e9d8b069d82aa4a70bfbdbf6020bf393
pravinpoudel <prvnpoudel4@gmail.com>
2020-03-15 15:59:33 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+<script>
+const swiper = new Swiper('.swiper-container', {
+  direction: 'vertical',
+  mousewheel: {},
+  effect: 'cube',
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false
+  }
+});
+</script>
